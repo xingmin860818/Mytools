f53945119decf8d4d6bdcb7e1a7f4a3d1bd4f587
xingmin860818 <763513751@qq.com>
2015-12-10 14:03:47 +0800

    更新可以备份目录按天创建

--- log_manage.py
+++ log_manage.py
@@ -1,4 +1,3 @@
-
 #-*-coding:utf8-*-
 import zipfile
 import time
@@ -7,54 +6,57 @@ import re
 import shutil
 
 class Logs(object):
-        def __init__(self,logpath):
-                self.logpath = logpath
-        #统计运行日志文件
-        def statisticsLog(self):
-                self.logfiles = []
-                os.chdir(self.logpath)
-                files = os.listdir(self.logpath)
-                for file in files:
-                        if re.match(r'.*\.log$',file):
-                                self.logfiles.append(file)
-                return self.logfiles
-        #切割日志文件，补当前日期，并且创建新日志文件，调用服务重启接口
-        def cutLog(self):
-                timestamp = time.strftime('%Y-%m-%d',time.localtime(time.time()))
-                for file in self.logfiles:
-                        os.rename(file,file+timestamp)
-                        os.mknod(file,0644)
-        #切割日志后进行服务重新加载（nginx）
-        def restartService(self,servicename):
-                command = 'service {} reload'.format(servicename)
-                #os.popen(command).readlines()
-                os.system(command)
-        #定义压缩文件函数
-        def zipLog(self):
-                os.chdir(self.logpath)
-                files = os.listdir(self.logpath)
-                for file in files:
-                        if re.match(r'.*\.log\d{4}-\d{2}-\d{2}$',file):
-                                f = zipfile.ZipFile(file+'.zip','w',zipfile.ZIP_DEFLATED)
-                                f.write(file)
-                                f.close()
-                                os.remove(file)
-        #备份压缩日志文件
-        def backup(self,backpath):
-                if not os.path.exists(backpath):
-                        os.mkdir(backpath,0755)
-                os.chdir(self.logpath)
-                files = os.listdir(self.logpath)
-                for file in files:
-                        if re.match(r'.*\.log\d{4}-\d{2}-\d{2}\.zip$',file):
-                                shutil.copy(file,backpath)
-                                os.remove(file)
+	def __init__(self,logpath):
+		self.logpath = logpath
+	#统计运行日志文件
+	def statisticsLog(self):
+		self.logfiles = []
+		os.chdir(self.logpath)
+		files = os.listdir(self.logpath)
+		for file in files:
+			if re.match(r'.*\.log$',file):
+				self.logfiles.append(file)
+		return self.logfiles
+	#切割日志文件，补当前日期，并且创建新日志文件，调用服务重启接口
+	def cutLog(self):
+		self.timestamp = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+		for file in self.logfiles:
+			os.rename(file,file+self.timestamp)
+			os.mknod(file,0644)
+	#切割日志后进行服务重新加载（nginx）
+	def restartService(self,servicename):
+		command = 'service {} reload'.format(servicename)
+		#os.popen(command).readlines()
+		os.system(command)
+	#定义压缩文件函数
+	def zipLog(self):
+		os.chdir(self.logpath)
+		files = os.listdir(self.logpath)
+		for file in files:
+			if re.match(r'.*\.log\d{4}-\d{2}-\d{2}$',file):
+				f = zipfile.ZipFile(file+'.zip','w',zipfile.ZIP_DEFLATED)
+				f.write(file)
+				f.close()
+				os.remove(file)
+	#备份压缩日志文件
+	def backup(self,backpath):
+		backdir = os.path.join(backpath,self.timestamp)
+		if not os.path.exists(backdir):
+			os.mkdir(backdir,0755)
+		os.chdir(self.logpath)
+		files = os.listdir(self.logpath)
+		for file in files:
+			if re.match(r'.*\.log\d{4}-\d{2}-\d{2}\.zip$',file):
+				shutil.copy(file,backdir)
+				os.remove(file)
+
 if __name__ == '__main__':
-        l = Logs('/var/log/nginx/')
-        l.statisticsLog()
-        l.cutLog()
-        l.restartService('nginx')
-        l.zipLog()
-        l.backup('/tmp')
+	l = Logs('/var/log/nginx/')
+	l.statisticsLog()
+	l.cutLog()
+	l.restartService('nginx')
+	l.zipLog()
+	l.backup('/tmp/')
+
 
         
