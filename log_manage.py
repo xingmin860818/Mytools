ea4128ecc8102f014da41ccbbec0e6cd7c56d086
xingmin860818 <763513751@qq.com>
2015-12-10 10:59:23 +0800

    抽象日志切割备份流程

--- log_manage.py
+++ log_manage.py
@@ -1,26 +1,51 @@
+
 #-*-coding:utf8-*-
 import zipfile
 import time
 import os
 import re
+import shutil
 
-#定义切割文件
-def cutLog(logdir):
-	timestamp = time.strftime('%Y-%m-%d',time.localtime(time.time()))
-	os.chdir(logdir)
-	files = os.listdir('.')
-	for file in files:
-		if re.match(r'.*\.log$',file):
-			os.rename(file,file+timestamp)
-			os.mkfifo(file)
-			break
-def restartService(servicename):
-	command = 'service {} reload'.format(servicename)
-	os.popen(command)
-#定义压缩文件函数
-def zipLog(filelist):
-	timestamp = timestamp = time.strftime('%Y-%m-%d',time.localtime(time.time()))
-	file = zipfile.ZipFile('xxxx.zip','w',zipfile.ZIP_DEFLATED)
-	file.write('file_to_add.log')
-	f.close()
-print restartService('/etc/init.d/nginx restart')
+class Logs(object):
+        def __init__(self,logpath):
+                self.logpath = logpath
+        #统计运行日志文件
+        def statisticsLog(self):
+                self.logfiles = []
+                os.chdir(self.logpath)
+                files = os.listdir(self.logpath)
+                for file in files:
+                        if re.match(r'.*\.log$',file):
+                                self.logfiles.append(file)
+                return self.logfiles
+        #切割日志文件，补当前日期，并且创建新日志文件，调用服务重启接口
+        def cutLog(self):
+                timestamp = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+                for file in self.logfiles:
+                        os.rename(file,file+timestamp)
+                        os.mknod(file,0644)
+        #切割日志后进行服务重新加载（nginx）
+        def restartService(self,servicename):
+                print os.listdir(self.logpath)
+                command = 'service {} reload'.format(servicename)
+                #os.popen(command).readlines()
+                os.system(command)
+        #定义压缩文件函数
+        def zipLog(self):
+                os.chdir(self.logpath)
+                files = os.listdir(self.logpath)
+                for file in files:
+                        if re.match(r'.*\.log\d{4}-\d{2}-\d{2}$',file):
+                                f = zipfile.ZipFile(file+'.zip','w',zipfile.ZIP_DEFLATED)
+                                f.write(file)
+                                f.close()
+                                os.remove(file)
+        #备份压缩日志文件
+        def backup(self,backpath):
+                if not os.path.exists(backpath):
+                        os.mkdir(backpath,0755)
+                os.chdir(self.logpath)
+                files = os.listdir(self.logpath)
+                for file in files:
+                        if re.match(r'.*\.log\d{4}-\d{2}-\d{2}\.zip$',file):
+                                shutil.copy(file,backpath)
